30ca357a3a891d6ffc3d24b20619f695c6ae802e
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-06-27 11:22:04 -0300

    first commit

---
+++ src/test/java/com/tidz/thymeleafdemo/ThymeleafdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidz.thymeleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
