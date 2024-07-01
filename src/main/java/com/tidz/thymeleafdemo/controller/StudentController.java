433eebc4b55055fec6319768755273beb97cdc91
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-07-01 14:27:22 -0300

    Add list of countries to the application.properties and retrieve it in the StudentController

--- src/main/java/com/tidz/thymeleafdemo/controller/StudentController.java
+++ src/main/java/com/tidz/thymeleafdemo/controller/StudentController.java
@@ -1,5 +1,8 @@
 package com.tidz.thymeleafdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +14,17 @@ import com.tidz.thymeleafdemo.model.Student;
 @Controller
 public class StudentController {
 
+	@Value("${countries}")
+	List<String> countries;
+
 	@GetMapping("/showStudentForm")
 	public String showForm(Model model) {
 		Student student = new Student();
 
 		model.addAttribute("student", student);
+
+		model.addAttribute("countries", countries);
+
 		return "student-form";
 	}
 
